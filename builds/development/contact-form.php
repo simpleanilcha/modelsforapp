64c0c985dd91c5e13c1c51527aec16943070947a
Anil Maharjan <simpleanilcha@gmail.com>
2016-05-18 17:16:12 +0545

    working on form validaiton to make more clean.

--- builds/development/contact-form.php
+++ builds/development/contact-form.php
@@ -1,9 +1,10 @@
 <?php 
-if(isset($_POST['Submit'])){
-// $to = "iamanilmhj@hotmail.com";
+
+if(isset($_POST['submit'])){
+$to = "iamanilmhj@hotmail.com";
 $from = $_POST['modelEmail'];			
 $subject = "Email from Models for App";
-$message = "<strong>".$_POST['modelName']."</strong> has send you the information.<br/><br/>"."<br/><br/>Sender: ".$from."Relevant Social Media Accounts: ".$_POST['modelSocial']."<br/>Number of followers: ".$_POST['modelFollowers'];
+$message = "<strong>".$_POST['modelName']."</strong> has send you the information.<br/><br/>"."Name: ".$_POST['modelName']."<br/>Email: ".$from."<br/>Relevant Social Media Accounts: ".$_POST['modelSocial']."<br/>Number of followers: ".$_POST['modelFollowers'];
 // To send HTML mail, the Content-type header must be set
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
@@ -14,6 +15,10 @@ $headers .= 'From: '.$from.'' . "\r\n";
 $headers .= 'Bcc: iamanilmhj@hotmail.com' . "\r\n";
 //print_r($message); die;      
 @mail($to,$subject,$message,$headers);
-echo "<script type='text/javascript'>alert('Thank you for your information. We will contact you Soon!');</script>";
+echo "<script type='text/javascript'>
+alert('Thank you for your information. We will contact you Soon!');
+window.location.href='http://websamplenow.com/53/modelsforapp2/mfa-details.html';
+</script>";
 	   }
+
 ?>
