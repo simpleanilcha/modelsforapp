36e40c0bc2485362ab81dc0cb9e09a53647ba575
Anil Maharjan <simpleanilcha@gmail.com>
2016-05-19 17:10:32 +0545

    contct form updated and uploaded in modelsforapp.com

--- builds/development/contact-form.php
+++ builds/development/contact-form.php
@@ -1,7 +1,7 @@
 <?php 
 
 if(isset($_POST['submit'])){
-$to = "iamanilmhj@hotmail.com";
+$to = "stephanie@modelsforapp.com";
 $from = $_POST['modelEmail'];			
 $subject = "Email from Models for App";
 $message = "<strong>".$_POST['modelName']."</strong> has send you the information.<br/><br/>"."Name: ".$_POST['modelName']."<br/>Email: ".$from."<br/>Relevant Social Media Accounts: ".$_POST['modelSocial']."<br/>Number of followers: ".$_POST['modelFollowers'];
@@ -17,7 +17,7 @@ $headers .= 'Bcc: iamanilmhj@hotmail.com' . "\r\n";
 @mail($to,$subject,$message,$headers);
 echo "<script type='text/javascript'>
 alert('Thank you for your information. We will contact you Soon!');
-window.location.href='http://websamplenow.com/53/modelsforapp2/mfa-details.html';
+window.location.href='http://modelsforapp.com/mfa-details.html';
 </script>";
 	   }
 
