51a58b6f41804089a563221a5eaf6928352e92b6
Anil Maharjan <simpleanilcha@gmail.com>
2016-05-24 13:10:15 +0545

    email receiver add to Tommy sir.

--- builds/development/contact-form.php
+++ builds/development/contact-form.php
@@ -1,7 +1,7 @@
 <?php 
 
 if(isset($_POST['submit'])){
-$to = ["stephanie@modelsforapp.com","tommyod46@hotmail.com"];
+$to = "stephanie@modelsforapp.com";
 $from = $_POST['modelEmail'];			
 $subject = "Email from Models for App";
 $message = "<strong>".$_POST['modelName']."</strong> has send you the information.<br/><br/>"."Name: ".$_POST['modelName']."<br/>Email: ".$from."<br/>Relevant Social Media Accounts: ".$_POST['modelSocial']."<br/>Number of followers: ".$_POST['modelFollowers'];
@@ -11,7 +11,7 @@ $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 // Additional headers
 //$headers .= 'To:iamanilmhj@hotmail' . "\r\n";
 $headers .= 'From: '.$from.'' . "\r\n";
-//$headers .= 'Cc: iamanilmhj@hotmail.com' . "\r\n";
+$headers .= 'Cc: tommyod46@hotmail.com' . "\r\n";
 $headers .= 'Bcc: iamanilmhj@hotmail.com' . "\r\n";
 //print_r($message); die;      
 @mail($to,$subject,$message,$headers);
